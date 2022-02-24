d1aa01d602ceffe152e75086f1d2f9b8324450f7
sammit52 <sammit52@kingshigh.school.nz>
2022-02-24 14:51:23 +1300

    Create 3_variables.py
    Practising variables

---
+++ 3_variables.py
@@ -0,0 +1,42 @@
+#1
+player_lives = 3
+#2
+login_name = "Samuel Mitchell"
+#3
+money = 0
+#4
+phone_number = "123 1234 1234 123"
+#5
+name = "Samuel"
+#6
+age = 14
+#7
+year = 2022
+#8
+print(name)
+#9
+print(age)
+#10
+print(year)
+#11
+surname = "Mitchell"
+#12
+school_name = "King's Highschool"
+#13
+favourite_food = "Macaroni & Cheese"
+#14
+pet_name = "Ginger"
+#15
+blocks_of_chocolates = 0
+#16
+player_score = 0
+#17
+player_highscore = 1000
+#18
+racing_laps_completed = 3
+#19
+ammunition = 56
+#20
+gear = 2
+#21
+speed = 56
