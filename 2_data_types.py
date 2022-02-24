5a0fc2005fe5592c095b15201227a14188609730
sammit52 <sammit52@kingshigh.school.nz>
2022-02-24 14:31:48 +1300

    Create 2_data_types.py
    Practising Data Types

---
+++ 2_data_types.py
@@ -0,0 +1,63 @@
+
+#1
+print("Oh, once again I am a child\nI let it all go (go), of everything that I know, yeah\nOf everything that I know, yeah\nAnd nothing hurts anymore, I feel kinda free\nWe're still the kids we used to be, yeah, yeah\nI put my hand on the stove, to see if I still bleed\nYeah, and nothing hurts anymore, I feel kinda free\nWe're still the kids we used to be, yeah, yeah\nI put my hand on the stove, to see if I still bleed\nYeah, and nothing hurts anymore, I feel kinda free\nWe're still the kids we used to be, yeah, yeah\nI put my hand on the stove, to see if I still bleed\nYeah, and nothing hurts anymore, I feel kinda free\nWe're still the kids we used to be, yeah, yeah\nI put my hand on the stove, to see if I still bleed\nYeah, and nothing hurts anymore, I feel kinda free")
+#2
+print("\"Not everyone can become a great artist, but a great artist can come from anywhere.” - Anton Ego")
+#3
+print("I believe that a string is a collection of characters.")
+#4
+print("An integer is a whole number.")
+#5
+print("A float is a number with decimal places.")
+#6
+print(543/3)
+#7
+print(int(543/3))
+#8
+print(543%3)
+#9
+print(1000%9)
+#10
+print(int(434/10))
+#11
+print(3%2)
+#12
+print(5%5)
+#13
+print(555/2)
+#14
+print(123/2)
+#15
+print(123//2)
+#16
+print(123%2)
+#17
+print(764%2)
+#18
+print(165//2)
+#19
+print(19.5+16.7)
+#20
+print(10+10)
+#21
+print(10-1.1)
+#22
+print(10.1-1)
+#23
+print(int(10/2))
+#24
+print(10*2.5)
+#25
+print(10*10)
+#26
+print("Hello World " *10)
+#27
+print("Hello World\n" *10)
+#28
+print("5+10=" + str(5+10))
+#29
+print("15/2=" + str(15/2))
+#30
+print("30/2*5=" + str(30/2*5))
+#31
+print("30/2*5=" + str(int(30/2*5)))
