2787a1ba079255ea1f74f51c35732c59e978cbe5
sammit52 <sammit52@kingshigh.school.nz>
2022-02-28 09:28:23 +1300

    Create 4_modifying_variables.py
    Practising variables

---
+++ 4_modifying_variables.py
@@ -0,0 +1,39 @@
+#1
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+#2
+print(money)
+#3
+print("Buying a hat")
+
+money = money - hat
+
+print(f"You now have {money} dollars")
+#4
+print("Buying a top")
+
+money = money - top
+
+print(f"You now have {money} dollars")
+#5
+print("Buying a pants")
+
+money = money - pants
+
+print(f"You now have {money} dollars")
+#6
+print("Buying a belt")
+
+money = money - belt
+
+print(f"You now have {money} dollars")
+#7
+print("Buying a shoes")
+
+money = money - shoes
+
+print(f"You now have {money} dollars")
